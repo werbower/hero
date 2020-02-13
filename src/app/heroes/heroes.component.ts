fdfe328b34f00da5285953b0a6d2ef71399dd76f
UserDev <UserDev>
2020-02-13 20:33:55 +0200

    initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Models/hero';
+import { HEROES } from '../services/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
